0e67e870b7ccb18b7b924a0dea132533f2d77293
unknown <austin.wold@W209-7010-07.oit.edu>
2013-04-17 20:40:06 -0700

    Set up ladder and double jump on ladder

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour {
 	public float movementSpeed = 6f;
 	public float jumpSpeed = 3f;
 	public float gravity = 10f;
+	bool ladderClimbing = false;
 	
 	private Vector3 movementVector = Vector3.zero;
 	
@@ -21,15 +22,37 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(characterController.isGrounded){
-			movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+			movementVector = new Vector3( Input.GetAxis("Horizontal"), 0, 0);
 			movementVector = transform.TransformDirection(movementVector);
 			movementVector *= movementSpeed;
-			if(Input.GetButton ("Jump"))
+			if(Input.GetButton ("Jump")){
 				movementVector.y = jumpSpeed;
-			
+			}
+		}		
+		if(ladderClimbing == true){
+			movementVector.x = Input.GetAxis("Horizontal");
+			movementVector.y = Input.GetAxis("Vertical");
+			movementVector *= movementSpeed;
+			if(Input.GetButton ("Jump")){
+				movementVector.y = jumpSpeed;	
+			}
 		}
 		movementVector.y -= gravity*Time.deltaTime;
-		
 		characterController.Move(movementVector*Time.deltaTime);
 	}
+	
+	void OnTriggerEnter (Collider characterController) {
+		if(characterController.gameObject.tag == "Ladder")
+		{
+			ladderClimbing = true;
+		}	
+	}
+	
+	void OnTriggerExit (Collider characterController) {
+		if(characterController.gameObject.tag == "Ladder")
+		{
+			ladderClimbing = false;
+		}
+	}
+	
 }
