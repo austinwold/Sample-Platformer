57c97d322c6d00053a44a71623eee9a9970e9c29
unknown <austin.wold@W209-7010-07.oit.edu>
2013-04-24 20:57:19 -0700

    added more editor window stuff

--- Assets/Editor/MyWindow.cs
+++ Assets/Editor/MyWindow.cs
@@ -1,16 +1,43 @@
+using UnityEditor;
 using UnityEngine;
 using System.Collections;
-using UnityEditor;
 
-public class MyWindow : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class MyWindow : EditorWindow {
+	float objMass = 1.00f;
 	
+	[MenuItem ("MyWindow/DoSomething")]
+	static void DoSomething() {
+		Debug.Log ("I did something");
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	[MenuItem ("MyWindow/Rigidbody/Double Mass")]
+    static void DoubleMass () {
+        GameObject body = Selection.gameObjects[0];
+		if (body.GetComponent<Rigidbody>() == null){
+			body.AddComponent<Rigidbody>();
+		}
+        body.rigidbody.mass = body.rigidbody.mass * 2;
+        Debug.Log ("Doubled Rigidbody's Mass to " + body.rigidbody.mass + " from Context Menu.");
+    }
 	
+	[MenuItem ("Window/MyWindow")]
+	public static void ShowWindow(){
+		EditorWindow.GetWindow(typeof(MyWindow));	
+	}
+
+	void OnGUI(){
+		GUILayout.Label ("Set object mass",EditorStyles.boldLabel);
+		objMass = EditorGUILayout.Slider("Slider", objMass, 1, 100);
+		if (Selection.activeObject == null){
+			Debug.Log ("Select an object");
+		}
+		else{
+			GameObject body = Selection.gameObjects[0];
+			if (body.GetComponent<Rigidbody>() == null){
+				body.AddComponent<Rigidbody>();
+			}
+		body.rigidbody.mass = objMass;
+		Debug.Log("Rigidbody mass altered to " + body.rigidbody.mass);
+		}	
 	}
 }
